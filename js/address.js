103480393750dd6611c995782e8fcd73da090fc3
dsky1990 <dutian1990@gmail.com>
2017-03-06 17:23:39 +0800

    update address

--- js/address.js
+++ js/address.js
@@ -1,3 +1,39 @@
 /**
  * Created by dsky on 2017/3/6.
  */
+const vm = new Vue({
+    el: '.checkout-addr',
+    data: {
+        addressList: [],
+        limitLen: 3,
+        currentIndex: 0,
+        shipType: 0
+    },
+    mounted: function () {
+        this.$nextTick(()=>{
+            this.renderData();
+        })
+    },
+    computed:{
+        addressFliter: function () {
+            return this.addressList.slice(0, this.limitLen);
+
+        }
+    },
+    methods:{
+        renderData: ()=>{
+            axios.get('./data/address.json').then((res)=>{
+                vm.addressList = res.data.result;
+            });
+        },
+        setDefault: (addressId)=>{
+            vm.addressList.forEach((address, index)=>{
+                if(address.addressId===addressId){
+                    address.isDefault = true;
+                }else {
+                    address.isDefault = false;
+                }
+            })
+        }
+    }
+});
